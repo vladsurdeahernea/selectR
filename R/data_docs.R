14880026e3153c5d27bc4ae7656b462dd6fbdbfc
vladsurdeahernea <vlad.surdea-hernea@boku.ac.at>
2025-08-15 10:11:43 +0300

    Update data_docs.R

--- R/data_docs.R
+++ R/data_docs.R
@@ -1,35 +1,25 @@
 #' Toy cross-section for caseselectR
 #'
 #' A tiny dataset with 40 units and variables Y, X, Z1, Z2.
-#' Designed for quick examples and tests.
-#'
 #' @format A data frame with 40 rows and 5 variables:
 #' \describe{
 #'   \item{unit}{unit id}
-#'   \item{Y}{numeric outcome}
 #'   \item{X}{main independent variable}
 #'   \item{Z1}{covariate}
 #'   \item{Z2}{covariate}
+#'   \item{Y}{numeric outcome}
 #' }
-#' @examples
-#' data(toy_cross, package = "caseselectR")
-#' str(toy_cross)
 "toy_cross"
 
 #' Toy panel for caseselectR
 #'
-#' A tiny panel with 10 units and 6 periods, variables Y, X, Z1.
-#' Good for longitudinal and index strategies.
-#'
+#' A tiny panel dataset with 10 units and 6 periods, variables Y, X, Z1.
 #' @format A data frame with 60 rows and 4 variables:
 #' \describe{
-#'   \item{unit}{unit id (factor or character)}
-#'   \item{t}{time index (integer)}
-#'   \item{Y}{numeric outcome}
+#'   \item{unit}{unit id}
+#'   \item{t}{time index}
 #'   \item{X}{main independent variable}
 #'   \item{Z1}{covariate}
+#'   \item{Y}{numeric outcome}
 #' }
-#' @examples
-#' data(toy_panel, package = "caseselectR")
-#' head(toy_panel)
 "toy_panel"
